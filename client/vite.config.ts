8ddf96346fde5f656411dca8bb005f192124aab2
Brandon Vargas <andres.vpineros@gmail.com>
2023-06-03 01:16:31 -0500

    setup tests, config eslint and prettier

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    css: true,
+    coverage: {
+      provider: 'c8',
+      all: true,
+      lines: 60,
+      functions: 70,
+      branches: 70,
+      statements: 70,
+    },
+  },
 })
